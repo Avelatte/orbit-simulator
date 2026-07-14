894da6b74dc8e38345cbbfeb741fe564516c368b
Aveline Cacciatore <70073218+Avelatte@users.noreply.github.com>
2026-07-14 17:22:27 -0300

    stuff closing in for today

--- js/sketch.js
+++ js/sketch.js
@@ -1,17 +1,30 @@
+const bg_color = window.getComputedStyle(document.documentElement).getPropertyValue('--main-bg-color').trim();
+const blue_astro_color = window.getComputedStyle(document.documentElement).getPropertyValue('--astro-blue').trim();
+
+// Array os celestial bodies
+let bodies = [];
+
 function setup() {
   createCanvas(windowWidth,windowHeight);
 }
 
 function draw() {
-  // Set background color based on CSS value
-  const bg_color = window.getComputedStyle(document.documentElement).getPropertyValue('--main-bg-color').trim();
-  const blue_astro_color = window.getComputedStyle(document.documentElement).getPropertyValue('--astro-blue').trim();
   background(bg_color);
 
   noFill();
   strokeWeight(4);
   stroke(blue_astro_color);
-  circle((windowWidth/2),(windowHeight/2),80);
+  circle((windowWidth/2),(windowHeight/2),75);
+}
+
+function mousePressed() {
+let new_body = {
+    type: 'circle',
+    x: mouseX,
+    y: mouseY,
+    size: 30,
+    color: color(blue_astro_color)
+  };
 }
 
 function windowResized() {
