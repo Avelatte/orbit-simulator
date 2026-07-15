79418c1c5bf2621038b85c934de2fe4307f471e9
Aveline Cacciatore <70073218+Avelatte@users.noreply.github.com>
2026-07-15 11:34:37 -0300

    body constructor and gravity

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,12 @@
 const bg_color = window.getComputedStyle(document.documentElement).getPropertyValue('--main-bg-color').trim();
 const blue_astro_color = window.getComputedStyle(document.documentElement).getPropertyValue('--astro-blue').trim();
 
-// Array os celestial bodies
+// Array of celestial bodies
 let bodies = [];
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
+  angleMode(DEGREES);
 }
 
 function draw() {
@@ -14,19 +15,44 @@ function draw() {
   noFill();
   strokeWeight(4);
   stroke(blue_astro_color);
-  circle((windowWidth/2),(windowHeight/2),75);
+  //circle((width/2),(height/2),75);
+  circle(500, 500, 75);
+
+  // Draw every body in the bodies array
+  for (let body of bodies){
+    let gravity = add_gravity(body.x, body.y, 500, 500, 1);
+
+    // Moving section
+    body.x += gravity[0];
+    body.y += gravity[1];
+
+    // Rendering section
+    let x = body.x;
+    let y = body.y;
+    let size = body.size;
+    stroke(body.color);
+    circle(x, y, size);
+  }
 }
 
 function mousePressed() {
-let new_body = {
-    type: 'circle',
-    x: mouseX,
-    y: mouseY,
-    size: 30,
-    color: color(blue_astro_color)
-  };
+  bodies.push(new Body(mouseX, mouseY, 30, blue_astro_color));
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+class Body {
+  constructor(x, y, size, color, spd){
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.color = color;
+  }
+}
+
+function add_gravity(x1, y1, x2, y2, grav) {
+  let v1 = createVector(x1, y1);
+  let v2 = createVector(x2, y2);
+  let angle = atan2(v2.y - v1.y, v2.x - v1.x);
+  let gravX = grav * cos(angle);
+  let gravY = grav * sin(angle);
+  return [gravX, gravY];
 }
